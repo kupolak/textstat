cba9da765eb8250a2bc303c3234938bd81cb4e99
Jakub Polak <jakub.polak.vz@gmail.com>
2018-11-02 19:56:14 +0100

    Add lexicon_count function

--- lib/textstat.rb
+++ lib/textstat.rb
@@ -3,4 +3,10 @@ class TextStat
     text = text.delete(' ') if ignore_spaces
     text.length
   end
+
+  def self.lexicon_count(text, remove_punctuation = true)
+    text  = text.gsub(/[^a-zA-Z\s]/, '').squeeze(' ') if remove_punctuation
+    count = text.split(' ').count
+    count
+  end
 end
