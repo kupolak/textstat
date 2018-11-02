97e4d2895831e3a941367cb49f220c97f5e59eed
Jakub Polak <jakub.polak.vz@gmail.com>
2018-11-02 20:32:28 +0100

    Add syllabe_count function

--- lib/textstat.rb
+++ lib/textstat.rb
@@ -1,3 +1,5 @@
+require 'text-hyphen'
+
 class TextStat
   def self.char_count(text, ignore_spaces = true)
     text = text.delete(' ') if ignore_spaces
@@ -7,6 +9,20 @@ class TextStat
   def self.lexicon_count(text, remove_punctuation = true)
     text  = text.gsub(/[^a-zA-Z\s]/, '').squeeze(' ') if remove_punctuation
     count = text.split(' ').count
+    count
+  end
+
+  def self.syllable_count(text, language = 'en_us')
+    return 0 if text.empty?
+
+    text = text.downcase
+    text.gsub(/[^a-zA-Z\s]/, '').squeeze(' ')
+    dictionary = Text::Hyphen.new(language: language, left: 0, right: 0)
+    count = 0
+    text.split(' ').each do |word|
+      word_hyphenated = dictionary.visualise(word)
+      count += [1, word_hyphenated.count('-') + 1].max
+    end
     count
   end
 end
